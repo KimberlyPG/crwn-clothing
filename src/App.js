1285b7ce074cfc759a9cd86236fd596a89605481
KimberlyPG <kim-kpgs98@hotmail.com>
2022-05-19 15:09:39 -0600

    creation of navigation component and home component and new style for navigation component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-    </div>
-  )
-}
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -19,8 +11,9 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} >
-        <Route path='shop' element={<Shop />}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} /> 
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   );  
