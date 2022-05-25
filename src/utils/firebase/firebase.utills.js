ccb889f4cbcaaf30c5afa8a1a3247e96c70852d0
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-05-25 17:59:04 -0600

    directory-item
    category item component was change to directory item component in order to fix some clashing styles

--- src/utils/firebase/firebase.utills.js
+++ src/utils/firebase/firebase.utills.js
@@ -7,7 +7,7 @@ import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signOut,
-    onAuthStateChanged
+    onAuthStateChanged,
 } from 'firebase/auth';
 
 // Import methods from firestore
@@ -16,7 +16,11 @@ import {
     getFirestore,
     doc, //allows us to retrieve documents inside our firestore database
     getDoc, //gettting document data
-    setDoc //setting document data
+    setDoc, //setting document data
+    collection,
+    writeBatch,
+    query,
+    getDocs
 } from 'firebase/firestore'
 
 // Web app's Firebase configuration
@@ -34,17 +38,51 @@ const firebaseConfig = {
 
 //  Initialize a provider 
   const provider = new GoogleAuthProvider(); //GoogleAuthProvider is a class that we get from firebase authentication
+  
   provider.setCustomParameters({
     prompt: "select_account"
   });
 
   export const auth = getAuth();
+  
   export const signInWithGooglePopup = () => {
     return signInWithPopup(auth, provider);
   };
+  export const signInWithGoogleRedirect = () => {
+    signInWithRedirect(auth, provider);
+  }
 
-//  Instance to get or set data to our document 
+  //  Instance to get or set data to our document 
   export const db = getFirestore();
+
+  // Add collection and documents to firestore
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db); //batch instance
+
+    objectsToAdd.forEach((object) => {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+      batch.set(docRef, object);
+    })
+
+    await batch.commit();
+    console.log('done');
+  }
+
+  // get collection from firestore
+  export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+      const { title, items } = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+
+    return categoryMap;
+  }
 
   export const createUserDocumentFromAuth = async (
     userAuth,
