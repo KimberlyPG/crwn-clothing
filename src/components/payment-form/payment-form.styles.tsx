04aebea73e06577111c045ea88ae259bec948117
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-06-08 00:26:17 -0600

    responsive page
    main page, shop page, category page and authentication page now have responsive design

--- src/components/payment-form/payment-form.styles.tsx
+++ src/components/payment-form/payment-form.styles.tsx
@@ -7,6 +7,7 @@ export const PaymentFormContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
 `;
 
 export const FormContainer = styled.form`
--- src/routes/category/category.styles.tsx
+++ src/routes/category/category.styles.tsx
@@ -5,6 +5,11 @@ export const CategoryContainer = styled.div`
     grid-template-columns: repeat(4, 1fr);
     column-gap: 20px;
     row-gap: 50px;
+    @media only screen and (max-width: 800px) {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-grap: 15px;
+      }
 `;
 export const Title = styled.h2`
     font-size: 20px;
